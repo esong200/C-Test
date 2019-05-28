a00dbe0ba1b3f54b75bf35c852410258e1aaca56
Ethan Song <e.song200@gmail.com>
2019-05-28 01:21:37 -0700

    Initial Commit

---
+++ C++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Test
+//
+//  Created by Ethan Song on 5/28/19.
+//  Copyright © 2019 Ethan Song. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
